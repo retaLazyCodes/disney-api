3dc092c0988377f6d7be12ff54923b580cca37b5
Brian Retamar <leo1632@hotmail.com>
2021-11-26 15:37:44 -0300

    Finished Characters crud!!

--- Disney.Api/Controllers/CharactersController.cs
+++ Disney.Api/Controllers/CharactersController.cs
@@ -32,6 +32,17 @@ namespace Disney.Api.Controllers
             return Ok(characterDto);
         }
         
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCharacterById(int id)
+        {
+            var character = await _characterService.GetCharacterById(id);
+            if (character != null)
+            {
+                return Ok(character);
+            }
+            return NotFound();
+        }
+        
         [HttpPost]
         public async Task<IActionResult> InsertCharacter(CharacterDto characterDto)
         {
@@ -41,6 +52,22 @@ namespace Disney.Api.Controllers
             characterDto = _mapper.Map<CharacterDto>(character);
             
             return  Ok(characterDto);
+        }
+        
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdatePost(int id, CharacterDto characterDto)
+        {
+            var character = _mapper.Map<Character>(characterDto);
+            character.Id = id;
+            var result = await _characterService.UpdateCharacter(character);
+            return Ok(result);
+        }
+        
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletePost(int id)
+        {
+            var result = await _characterService.DeleteCharacter(id);
+            return Ok(result);
         }
     }
 }
