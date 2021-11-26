c52af281056b7687d74cfd8ab04b7dd2793dbd0d
Brian Retamar <leo1632@hotmail.com>
2021-11-26 15:28:54 -0300

    Added POST method

--- Disney.Api/Controllers/CharactersController.cs
+++ Disney.Api/Controllers/CharactersController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using AutoMapper;
 using Disney.Core.DTOs;
+using Disney.Core.Entities;
 using Disney.Core.Interfaces;
 using Disney.Core.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,17 @@ namespace Disney.Api.Controllers
             var characterDto = _mapper.Map<IEnumerable<CharacterDto>>(characters);
             
             return Ok(characterDto);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> InsertCharacter(CharacterDto characterDto)
+        {
+            var character = _mapper.Map<Character>(characterDto);
+            await _characterService.InsertCharacter(character);
+
+            characterDto = _mapper.Map<CharacterDto>(character);
+            
+            return  Ok(characterDto);
         }
     }
 }
