46d8ba72d54ae29061603c247b53960389fb7ab6
Brian Retamar <leo1632@hotmail.com>
2021-11-26 23:10:00 -0300

    Created DTOs

---
+++ Disney.Core/DTOs/MovieDto.cs
@@ -0,0 +1,14 @@
+using System;
+using Disney.Core.Enumerations;
+
+namespace Disney.Core.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Score Score { get; set; }
+    }
+}
