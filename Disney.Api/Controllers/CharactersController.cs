ce422a8ba1702b8d356085f01a69f6705334269a
Brian Retamar <leo1632@hotmail.com>
2021-12-03 15:49:48 -0300

    Implemented Operation Result pattern

--- Disney.Api/Controllers/CharactersController.cs
+++ Disney.Api/Controllers/CharactersController.cs
@@ -29,22 +29,24 @@ namespace Disney.Api.Controllers
         {
             var characters = await _characterService.GetCharacters();
             var characterViewModels = _mapper.Map<IEnumerable<CharacterViewModel>>(characters);
-            var response = new ApiResponse<IEnumerable<CharacterViewModel>>(characterViewModels);
+            var response =
+                OperationResult<IEnumerable<CharacterViewModel>>.CreateSuccessResult(characterViewModels);
             return Ok(response);
         }
-        
+
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetCharacterById(int id)
         {
             var character = await _characterService.GetCharacterById(id);
             if (character != null)
             {
-                var response = new ApiResponse<Character>(character);
+                var response =
+                    OperationResult<Character>.CreateSuccessResult(character);
                 return Ok(response);
             }
             return NotFound();
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> InsertCharacter(CharacterDto characterDto)
         {
@@ -52,25 +54,26 @@ namespace Disney.Api.Controllers
             await _characterService.InsertCharacter(character);
 
             characterDto = _mapper.Map<CharacterDto>(character);
-            var response = new ApiResponse<CharacterDto>(characterDto);
+            var response =
+                OperationResult<CharacterDto>.CreateSuccessResult(characterDto);
             return Ok(response);
         }
-        
+
         [HttpPut("{id:int}")]
         public async Task<IActionResult> UpdateCharacter(int id, CharacterDto characterDto)
         {
             var character = _mapper.Map<Character>(characterDto);
             character.Id = id;
             var result = await _characterService.UpdateCharacter(character);
-            var response = new ApiResponse<bool>(result);
+            var response = OperationResult<bool>.CreateSuccessResult(result);
             return Ok(response);
         }
-        
+
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> DeleteCharacter(int id)
         {
             var result = await _characterService.DeleteCharacter(id);
-            var response = new ApiResponse<bool>(result);
+            var response = OperationResult<bool>.CreateSuccessResult(result);
             return Ok(response);
         }
     }
---
+++ Disney.Api/Responses/OperationResult.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Disney.Api.Responses
+{
+    public class OperationResult<TResult>
+    {
+        private OperationResult()
+        {
+        }
+
+        public bool Success { get; private set; }
+        public TResult Result { get; private set; }
+        public string NonSuccessMessage { get; private set; }
+        public Exception Exception { get; private set; }
+
+        public static OperationResult<TResult> CreateSuccessResult(TResult result)
+        {
+            return new OperationResult<TResult> { Success = true, Result = result };
+        }
+
+        public static OperationResult<TResult> CreateFailure(string nonSuccessMessage)
+        {
+            return new OperationResult<TResult> { Success = false, NonSuccessMessage = nonSuccessMessage };
+        }
+
+        public static OperationResult<TResult> CreateFailure(Exception ex)
+        {
+            return new OperationResult<TResult>
+            {
+                Success = false,
+                NonSuccessMessage = String.Format("{0}{1}{1}{2}", ex.Message, Environment.NewLine, ex.StackTrace),
+                Exception = ex
+            };
+        }
+    }
+}
