233d57d3f3a68e0a23da4ebe59407ef7ffb98dd3
Brian Retamar <leo1632@hotmail.com>
2021-12-24 05:19:17 -0300

    Added validation on query filters

--- Disney.Core/QueryFilters/CharacterQueryFilter.cs
+++ Disney.Core/QueryFilters/CharacterQueryFilter.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Disney.Core.QueryFilters
 {
     public class CharacterQueryFilter
@@ -5,7 +7,11 @@ namespace Disney.Core.QueryFilters
         public string Name { get; set; }
         public int? Age { get; set; }
         public int? Movie { get; set; }
+        
+        [Range(0, int.MaxValue, ErrorMessage = "PageSize must be a value bigger or equal than {1}")]
         public int PageSize { get; set; } 
+        
+        [Range(0, int.MaxValue, ErrorMessage = "PageNumber must be a value bigger or equal than {1}")]
         public int PageNumber { get; set; }
 
     }
